c861b95e31a859cafec8cbe8e5a46bad769f6bdc
Krzysztof Kiec <kkiec95@gmail.com>
2022-03-04 17:44:25 +0100

    Add stripe webhooks

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   get 'plans', to: 'static_pages#plans'
   post 'checkout/create', to: 'checkout#create', as: 'checkout_create'
   post 'billing_portal/create', to: 'billing_portal#create', as: 'billing_portal_create'
+  post 'webhooks', to: 'webhooks#create'
 end
